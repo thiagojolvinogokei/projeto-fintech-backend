b5faca16b3acef6d2962c472ca92f1776e9ef728
thiagojolvinogokei <thiagojolvino@gokeitecnologia.com.br>
2025-03-02 02:20:06 -0300

    criação da classe de transação para suporte nos métodos da classe usuario

--- src/Transacao.java
+++ src/Transacao.java
@@ -1,5 +1,25 @@
+import java.util.Date;
+
 public class Transacao {
     private String id;
-    private String password;
-    
+    private double valor;
+    private String tipo;
+    private Usuario usuario;
+    private String contaRecebedora;
+
+    public Transacao(String id, double valor, String tipo, Usuario usuario, String contaRecebedora) {
+        this.id = id;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.usuario = usuario;
+        this.contaRecebedora = contaRecebedora;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Transação: " + id);
+        System.out.println("Valor: " + valor);
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Usuário: " + usuario.getNomeUsuario());
+        System.out.println("Conta Recebedora: " + contaRecebedora);
+    }
 }
